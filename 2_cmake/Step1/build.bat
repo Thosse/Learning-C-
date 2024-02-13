70c1d376572e563035822b8e867ff5641521bb71
Sasse, Thomas (DI FA SEA ES&PRC CHE2) <thomas.sasse@siemens.com>
2024-02-13 08:10:10 +0100

    Compile and link Code (Cmake Command)

--- 2_cmake/Step1/build.bat
+++ 2_cmake/Step1/build.bat
@@ -1,2 +1,4 @@
-REM IT is necessary, to specify the Generator in Windows
+REM Mustspecify the Generator with -G (at least in Windows)
+REM this builds the makefile
 cmake -B build -S./ -G "Unix Makefiles"
+cmake --build build
